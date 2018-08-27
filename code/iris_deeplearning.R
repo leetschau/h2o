f28cbb43090d181689e01084607043116b036c26
Li Chao <leetschau@gmail.com>
2018-08-27 17:39:19 +0800

    chapter 1 finished

--- code/iris_deeplearning.R
+++ code/iris_deeplearning.R
@@ -1,8 +1,9 @@
 library(h2o)
 h2o.init(nthreads = -1)
 
-datasets <- "https://raw.githubusercontent.com/DarrenCook/h2o/bk/datasets/"
-data <- h2o.importFile(paste0(datasets,"iris_wheader.csv"))
+datasrc <- "https://raw.githubusercontent.com/DarrenCook/h2o/bk/datasets/"
+fullpath <- paste0(datasrc, "iris_wheader.csv")
+data <- h2o.importFile(fullpath)
 y <- "class"
 x <- setdiff(names(data), y)
 parts <- h2o.splitFrame(data, 0.8)
@@ -11,3 +12,4 @@ test <- parts[[2]]
 
 m <- h2o.deeplearning(x, y, train)
 p <- h2o.predict(m, test)
+h2o.performance(m, test)
